7324bdeac35814b054ab556b21de3ad7d28122fa
Emanuel Pituch <epituch@purdue.edu>
2019-02-13 18:31:54 -0500

    Added dark theme!

--- src/AccelerometerGyroGraphs.py
+++ src/AccelerometerGyroGraphs.py
@@ -1,40 +1,31 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import time
-import numpy as np
 
-fig = plt.figure()
+# DARK THEME!!!!!
+plt.style.use('dark_background')
 
-# def animate(i):
-#     pullData = open("sampleText.txt","r").read()
-#     dataArray = pullData.split('\n')
-#     xar = [5,6,7,8,9,34,12,13,43]
-#     yar = [5,6,7,3,1,3,4,2,1,2]
-#     for eachLine in dataArray:
-#         if len(eachLine)>1:
-#             x,y = eachLine.split(',')
-#             xar.append(int(x))
-#             yar.append(int(y))
-#     ax1.clear()
-#     ax1.plot(xar,yar)
-# ani = animation.FuncAnimation(fig, animate, interval=1000)
-t = np.arange(0, 50, 1)
-s = [5,7,10,15]
+# Graph 6 plots in a 2x3 fashion
 fig, axs = plt.subplots(nrows=2, ncols=3, sharex=True, sharey=True)
+
+# Adjust the space so there is more space  
+plt.tight_layout()
+plt.subplots_adjust(wspace=0, hspace=0)
+
 plt.axes(axs[0,0])
 plt.plot([5,7,10,15])
 plt.ylabel("Acceleration (m/s^2)")
-plt.xlabel("Balloon")
+plt.title("Balloon")
 plt.axes(axs[1,0])
 plt.ylabel("Gyro (degrees/s)")
 plt.plot([5,10,15])
 plt.axes(axs[0,1])
-plt.xlabel("Rocket")
+plt.title("Rocket")
 plt.plot([5,10,15])
 plt.axes(axs[1,1])
 plt.plot([5,10,15])
 plt.axes(axs[0,2])
-plt.xlabel("Stratologger")
+plt.title("Stratologger")
 plt.plot([5,10,15])
 plt.axes(axs[1,2])
 plt.plot([5,10,15])
