569790f736711387430afd3022fae1835ba32ed9
Emanuel Pituch <epituch@purdue.edu>
2019-02-18 23:25:08 -0500

    Added data points for each graph

--- src/AccelerometerGyroGraphs.py
+++ src/AccelerometerGyroGraphs.py
@@ -1,6 +1,26 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import queue
 import time
+
+def update_baloon_acc(x, y, z):
+    return 0
+
+def update_baloon_gyro(x, y, z):
+    return 0
+
+def update_rocket_acc(x, y, z):
+    return 0
+
+def update_rocket_gyro(x, y, z):
+    return 0
+
+def update_strato_acc(x, y, z):
+    return 0
+
+def update_strato_gyro(x, y, z):
+    return 0
+
 
 # DARK THEME!!!!!
 plt.style.use('dark_background')
@@ -12,24 +32,89 @@ fig, axs = plt.subplots(nrows=2, ncols=3, sharex=True, sharey=True)
 plt.tight_layout()
 plt.subplots_adjust(wspace=0, hspace=0)
 
-#Setup titles and axis labels
+# Start in interactive mode so that the graph starts in a non blocking thread
+plt.ion()
 
-plt.axes(axs[0,0])
-plt.plot([5,7,10,15])
+# Create several queue that holds the number for each line in every graph
+baloon_acc_xQ = queue.Queue()
+baloon_acc_yQ = queue.Queue()
+baloon_acc_zQ = queue.Queue()
+baloon_gyro_xQ = queue.Queue()
+baloon_gyro_yQ = queue.Queue()
+baloon_gyro_zQ = queue.Queue()
+rocket_acc_xQ = queue.Queue()
+rocket_acc_yQ = queue.Queue()
+rocket_acc_zQ = queue.Queue()
+rocket_gyro_xQ = queue.Queue()
+rocket_gyro_yQ = queue.Queue()
+rocket_gyro_zQ = queue.Queue()
+strato_acc_xQ = queue.Queue()
+strato_acc_yQ = queue.Queue()
+strato_acc_zQ = queue.Queue()
+strato_gyro_xQ = queue.Queue()
+strato_gyro_yQ = queue.Queue()
+strato_gyro_zQ = queue.Queue()
+
+# Fill the queue with 0s so that we can graph something if the user opens the graph early
+amount_of_point_to_graph = 20
+for i in range(0, amount_of_point_to_graph):
+    baloon_acc_xQ.put(0)
+    baloon_acc_yQ.put(0)
+    baloon_acc_zQ.put(0)
+    baloon_gyro_xQ.put(0)
+    baloon_gyro_yQ.put(0)
+    baloon_gyro_zQ.put(0)
+    rocket_acc_xQ.put(0)
+    rocket_acc_yQ.put(0)
+    rocket_acc_zQ.put(0)
+    rocket_gyro_xQ.put(0)
+    rocket_gyro_yQ.put(0)
+    rocket_gyro_zQ.put(0)
+    strato_acc_xQ.put(0)
+    strato_acc_yQ.put(0)
+    strato_acc_zQ.put(0)
+    strato_gyro_xQ.put(0)
+    strato_gyro_yQ.put(0)
+    strato_gyro_zQ.put(0)
+
+#Setup titles, axis labels, and plot the initial 0s
+baloon_acc_graph = plt.sca(axs[0,0])
+plt.plot(list(baloon_acc_xQ.queue))
+plt.plot(list(baloon_acc_yQ.queue))
+plt.plot(list(baloon_acc_zQ.queue))
 plt.ylabel("Acceleration (m/s^2)")
 plt.title("Balloon")
-plt.axes(axs[1,0])
+
+baloon_gyro_graph = plt.sca(axs[1,0])
+plt.plot(list(baloon_gyro_xQ.queue))
+plt.plot(list(baloon_gyro_yQ.queue))
+plt.plot(list(baloon_gyro_zQ.queue))
 plt.ylabel("Gyro (degrees/s)")
-plt.plot([5,10,15])
-plt.axes(axs[0,1])
+
+rocket_acc_graph = plt.sca(axs[0,1])
 plt.title("Rocket")
-plt.plot([5,10,15])
-plt.axes(axs[1,1])
+plt.plot(list(rocket_acc_xQ.queue))
+plt.plot(list(rocket_acc_yQ.queue))
+plt.plot(list(rocket_acc_zQ.queue))
+
+rocket_gyro_graph = plt.sca(axs[1,1])
 plt.xlabel("Time (s)")
-plt.plot([5,10,15])
-plt.axes(axs[0,2])
+plt.plot(list(rocket_gyro_xQ.queue))
+plt.plot(list(rocket_gyro_yQ.queue))
+plt.plot(list(rocket_gyro_zQ.queue))
+
+stato_acc_graph = plt.sca(axs[0,2])
 plt.title("Stratologger")
-plt.plot([5,10,15])
-plt.axes(axs[1,2])
-plt.plot([5,10,15])
-plt.show()
+plt.plot(list(strato_acc_xQ.queue))
+plt.plot(list(strato_acc_yQ.queue))
+plt.plot(list(strato_acc_zQ.queue))
+
+plt.sca(axs[1,2])
+plt.plot(list(strato_gyro_xQ.queue))
+plt.plot(list(strato_gyro_yQ.queue))
+plt.plot(list(strato_gyro_zQ.queue))
+
+# Run the loop so the graph gets updated every second
+while True:
+    plt.pause(0.001)
+    time.sleep(1)
