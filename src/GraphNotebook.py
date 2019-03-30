369965d07e3f51b7e6481ef1b09c70f89dc7c592
kcsodetz <kensodetz@gmail.com>
2019-03-30 16:59:06 -0400

    Fix merge

--- src/GraphNotebook.py
+++ src/GraphNotebook.py
@@ -27,34 +27,33 @@ class GraphNotebook(ttk.Notebook):
         event.widget.configure(height=tab.winfo_reqheight())
 
 
-if __name__ == "__main__":
-    example = Tk()
-    example.title("Example of Graphing Notebook")
-    example.geometry("300x300")
-
-    notebook = GraphNotebook(0, 0, 2, 5, example)
-    notebook.add_tab("Test Tab", Label(text="Test"))
-
-    example.mainloop()
-=======
-        custom_style = ttk.Style()
-        custom_style.configure('Custom.TNotebook.Tab', width=20, padding=[0, 40], font=('System', 8))
-        custom_style.configure('Custom.TNotebook', tabposition='wn')
-
-        # Initialize Notebook with Custom Style
-        self.graphs = ttk.Notebook(master, style='Custom.TNotebook')
-        # self.bind("<<NotebookTabChanged>>", self._on_tab_changed)
-
-        # Based on input, configuring and place
-        self.graphs.config(height=height)
-        self.graphs.grid(row=row, column=col, rowspan=span)
-
-    def add_tab(self, title, *widgets):
-        f = tk.Frame(self.graphs, width=200, height=200)
-        self.graphs.add(f, text=title)
-
-    def _on_tab_changed(self, event):
-        event.widget.update_idletasks()
-
-        tab = event.widget.nametowidget(event.widget.select())
-        event.widget.configure(height=tab.winfo_reqheight())
+# if __name__ == "__main__":
+#     example = Tk()
+#     example.title("Example of Graphing Notebook")
+#     example.geometry("300x300")
+#
+#     notebook = GraphNotebook(0, 0, 2, 5, example)
+#     notebook.add_tab("Test Tab", Label(text="Test"))
+#
+#     example.mainloop()
+#         custom_style = ttk.Style()
+#         custom_style.configure('Custom.TNotebook.Tab', width=20, padding=[0, 40], font=('System', 8))
+#         custom_style.configure('Custom.TNotebook', tabposition='wn')
+#
+#         # Initialize Notebook with Custom Style
+#         self.graphs = ttk.Notebook(master, style='Custom.TNotebook')
+#         # self.bind("<<NotebookTabChanged>>", self._on_tab_changed)
+#
+#         # Based on input, configuring and place
+#         self.graphs.config(height=height)
+#         self.graphs.grid(row=row, column=col, rowspan=span)
+#
+#     def add_tab(self, title, *widgets):
+#         f = tk.Frame(self.graphs, width=200, height=200)
+#         self.graphs.add(f, text=title)
+#
+#     def _on_tab_changed(self, event):
+#         event.widget.update_idletasks()
+#
+#         tab = event.widget.nametowidget(event.widget.select())
+#         event.widget.configure(height=tab.winfo_reqheight())
