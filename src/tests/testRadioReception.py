4afdade95f8a0c06c0d0c3272003224f3b5f7c8d
kcsodetz <kensodetz@gmail.com>
2019-04-06 14:39:59 -0400

    Add test script for radio

--- src/tests/testRadioReception.py
+++ src/tests/testRadioReception.py
@@ -1,4 +1,7 @@
 import json
+import time
+import traceback
+import sys
 
 from digi.xbee.devices import XBeeDevice, XBee64BitAddress, RemoteXBeeDevice, XBeeException
 
@@ -7,19 +10,19 @@ from digi.xbee.devices import XBeeDevice, XBee64BitAddress, RemoteXBeeDevice, XB
 # - For windows, it will be 'COM#'
 #
 # where # is the port number.
-LOCAL_PORT = "/dev/ttyS7"
+LOCAL_PORT = "/dev/ttyS13"
 
 # Baud rate of the local device
 BAUD_RATE = 9600
 
-DATA_TO_SEND = "Hello XBee!"
 
 # Remote node MAC address in hexadecimal format
-REMOTE_NODE_ADDRESS = "0013A2004148887C"
+REMOTE_NODE_ADDRESS = '0013A20040F6E10C'
 
 class RadioTest:
     def __init__(self):
         self.device = XBeeDevice(LOCAL_PORT, BAUD_RATE)
+        self.device.set_sync_ops_timeout(10)
         self.device.open()
         self.remote_device = None
         self.queue = None
@@ -28,21 +31,38 @@ class RadioTest:
             self.remote_device = RemoteXBeeDevice(self.device, XBee64BitAddress.from_hex_string(REMOTE_NODE_ADDRESS))
             if self.remote_device is None:
                 print("Could not find the remote device")
+
+            print(self.remote_device)
         except XBeeException:
             print("Exception has occurred")
 
     def spam(self):
         for i in range(100):
-            data = '{\"origin\":\"balloon\",\"alt\":' + i + ',\"GPS\":{\"long\":10,\"lat\":10},\"gyro\":{\"x\":10,\"y\":10,\"z\":10},\"mag\":10,\"temp\":10,\"acc\":{\"x\":10,\"y\":10,\"z\":10}}'
+            if i % 2 == 0:
+                origin = "balloon"
+            else:
+                origin = "rocket"
+
+
+            data = '{\"origin\":\"' + origin + '\",\"alt\":' + str(i) + ',\"GPS\":{\"long\":10,\"lat\":10},\"gyro\":{\"x\":10,\"y\":10,\"z\":' + str(i) + '},\"mag\": '+ str(i) + ',\"temp\": '+ str(i) +',\"acc\":{\"x\":10,\"y\":10,\"z\":10}}'
 
             print(data)
 
             try:
                 self.device.send_data(self.remote_device, data)
+                print("Success")
             except Exception as e:
+                traceback.print_exc()
+                self.device.reset()
+                self.device.close()
+                self.device.open()
                 print(e)
 
-            sleep(1)
+            time.sleep(2)
+
+        self.close()
+        sys.exit(1)
+
 
     def close(self):
         try:
